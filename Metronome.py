785bfbc0e2637776b3785f5e6b0016e0041a1db2
Brenne_Joshua <brenj644@hsd.k12.or.us>
2017-06-01 11:40:18 -0700

    adding exceptions, measure

--- Metronome.py
+++ Metronome.py
@@ -2,41 +2,63 @@ import time
 import winsound
 
 
-def playTick():
-    Tick = 'C:/Users/Steve/Desktop/Metronome/Tick.wav'
+def playTick(): # first beat
+    Tick = 'C:/Users/brenj644/Desktop/Metronome/Tick.wav'
     return winsound.PlaySound(Tick, winsound.SND_ASYNC)
 
-def playTock():
-    Tock = 'C:/Users/Steve/Desktop/Metronome/Tock.wav'
+def playTock(): # subsequent beats
+    Tock = 'C:/Users/brenj644/Desktop/Metronome/Tock.wav'
     return winsound.PlaySound(Tock, winsound.SND_ASYNC)
 
-    
-bpm = 60/int(input('How many beats per minute?: '))
-time_sig = int(input('How many beats per measure?: '))
-measure = int(input('How many measures?: '))
-
-
-n = 0 # for time signature
-for x in range(measure):
+def Function():
     while True:
-        if n == 0:
-            print(n+1)
-            n += 1
-            playTick()
-            time.sleep(bpm)
-                    
-        elif n < time_sig and n > 0:
-            print(n+1)
-            n += 1
-            playTock()
-            time.sleep(bpm)
+        try:
+            bpm = 60/int(input('How many beats per minute?: ')) # ask for bpm
+            break
+        except ZeroDivisionError: # if entered 0
+            print('\nPlease enter a whole number')
+        except ValueError: # if entered anything not a number
+            print('\nPlease enter a whole number')
 
-        else:
-            n = 0
-            x += 1
-            
-    if x > measure:
-        break
+    while True:
+        try:
+            time_sig = int(input('\nHow many beats per measure?: ')) # ask for time signature
+            break
+        except ValueError: # if entered anything not a number
+            print('\nPlease enter a whole number')
+        except time_sig <= 0: # if entered 0
+            print('\nPlease enter a whole number')
 
+    while True:
+        try:
+            measure = int(input('\nHow many measures?: ')) # ask for measure
+            break
+        except ValueError: # if entered anything not a number
+            print('\nPlease enter a whole number')
+        except measure <= 0: # if entered 0
+            print('\nPlease enter a whole number')
 
+    for i in range(measure): # play the metronome
+        n = 0 # for time signature
+        x = 0 # for measure
+        for j in range(time_sig):
+            if x <= measure:
+                if n == 0: # first beat
+                    print(n+1)
+                    n += 1
+                    playTick()
+                    time.sleep(bpm)
 
+                elif n < time_sig and n > 0: # middle beats
+                    print(n+1)
+                    n += 1
+                    playTock()
+                    time.sleep(bpm)
+
+                else: # last beat
+                    x += 1
+                    n = 0
+    print(' ')
+    Function()
+
+Function()
