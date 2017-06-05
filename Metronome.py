4dc33f2be3d6a2769d6753c8eb225a652b20a8c0
Brenne_Joshua <brenj644@hsd.k12.or.us>
2017-06-05 11:37:58 -0700

    added error definition and fixed limits

--- Metronome.py
+++ Metronome.py
@@ -1,5 +1,5 @@
-import time
-import winsound
+import time # for measuring time between ticks
+import winsound # for playing audio
 
 
 def playTick(): # first beat
@@ -10,33 +10,44 @@ def playTock(): # subsequent beats
     Tock = 'C:/Users/brenj644/Desktop/Metronome/Tock.wav'
     return winsound.PlaySound(Tock, winsound.SND_ASYNC)
 
-def Function():
+def error(): # for errors
+    print('\nPlease enter a number greater than zero.')
+
+def function(): # main function
     while True:
         try:
-            bpm = 60/int(input('How many beats per minute?: ')) # ask for bpm
-            break
-        except ZeroDivisionError: # if entered 0
-            print('\nPlease enter a whole number')
+            bpm = 60/int(input('\nHow many beats per MINUTE?: ')) # ask for bpm
+            if bpm < 0: # if entered anything less than zero
+                error()
+            elif bpm < 60/350 and bpm > 0: # if entered more than 350, as anything faster than 350 results in audio glitches
+                bpm = 60/350
+                break
+            else:
+                break
+        except ZeroDivisionError: # if entered zero
+            error()
         except ValueError: # if entered anything not a number
-            print('\nPlease enter a whole number')
+            error()
 
     while True:
         try:
-            time_sig = int(input('\nHow many beats per measure?: ')) # ask for time signature
-            break
+            time_sig = int(input('\nHow many beats per MEASURE?: ')) # ask for time signature
+            if time_sig <= 0: # if entered anything less than or equal to zero
+                error()
+            else:
+                break
         except ValueError: # if entered anything not a number
-            print('\nPlease enter a whole number')
-        except time_sig <= 0: # if entered 0
-            print('\nPlease enter a whole number')
+            error()
 
     while True:
         try:
             measure = int(input('\nHow many measures?: ')) # ask for measure
-            break
+            if measure <= 0: # if entered anything less than or equal to zero
+                error()
+            else:
+                break
         except ValueError: # if entered anything not a number
-            print('\nPlease enter a whole number')
-        except measure <= 0: # if entered 0
-            print('\nPlease enter a whole number')
+            error()
 
     for i in range(measure): # play the metronome
         n = 0 # for time signature
@@ -58,7 +69,9 @@ def Function():
                 else: # last beat
                     x += 1
                     n = 0
-    print(' ')
-    Function()
 
-Function()
+    print(' ')
+    function()
+
+
+function()
